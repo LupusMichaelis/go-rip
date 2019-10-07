5d30a8c03d6b8c7a5906c816bded8bc5022c22d7
Mickaël Wolff <m@lupusmic.org>
2019-10-07 16:50:55 +0200

    Make configuration immutable

--- main.go
+++ main.go
@@ -7,27 +7,31 @@ import (
 	"net/http"
 )
 
-func getConf(key string) (value string) {
+type Configuration struct {
+	Ip string
+	Port int16
+	Certificate string
+	Key string
+}
 
-	config := map[string]string{
-		"ip":          "::1",
-		"port":        "4343",
-		"certificate": "server.crt",
-		"key":         "server.key",
+var getConfiguration = (func () (func () Configuration) {
+	configuration := Configuration{
+		Ip:          "::1",
+		Port:        4343,
+		Certificate: "server.crt",
+		Key:         "server.key",
 	}
 
-	value = config[key]
-
-	return
-}
+	return (func() Configuration { return configuration })
+})()
 
 func main() {
 
-	addr := fmt.Sprintf("[%s]:%s", getConf("ip"), getConf("port"))
+	addr := fmt.Sprintf("[%s]:%d", getConfiguration().Ip, getConfiguration().Port)
 
 	srv := &http.Server{Addr: addr, Handler: http.HandlerFunc(handle)}
 	log.Printf("Serving on '%s'", addr)
-	log.Fatal(srv.ListenAndServeTLS(getConf("certificate"), getConf("key")))
+	log.Fatal(srv.ListenAndServeTLS(getConfiguration().Certificate, getConfiguration().Key))
 }
 
 func handle(w http.ResponseWriter, r *http.Request) {
