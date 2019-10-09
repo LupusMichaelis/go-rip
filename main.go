e224d686e246a5f45b1893a7769c647ded116104
Mickaël Wolff <m@lupusmic.org>
2019-10-09 10:29:28 +0200

    Move REST API in its own module

--- main.go
+++ main.go
@@ -3,48 +3,29 @@ package main
 import (
 	"fmt"
 
-	"github.com/ant0ine/go-json-rest/rest"
 	"log"
 	"net/http"
 
 	"./config"
+	"./rest"
 )
-
-type httpHandler func(rest.ResponseWriter, *rest.Request)
 
 func main() {
 
-	api := rest.NewApi()
-	api.Use(rest.DefaultDevStack...)
-	api.SetApp(rest.AppSimple(compose([]httpHandler{logger, kevin})))
-	apiHandler := api.MakeHandler()
+	restApi, err := rest.MakeRouter()
+	if nil != err {
+
+		log.Fatal(err)
+	}
+
+	apiHandler := restApi.MakeHandler()
 
 	addr := fmt.Sprintf("[%s]:%d", config.GetConfiguration().Ip, config.GetConfiguration().Port)
 	srv := &http.Server{
 		Addr:    addr,
 		Handler: apiHandler,
 	}
+
 	log.Printf("Serving on '%s'", addr)
 	log.Fatal(srv.ListenAndServeTLS(config.GetConfiguration().Certificate, config.GetConfiguration().Key))
-}
-
-func logger(w rest.ResponseWriter, r *rest.Request) {
-
-	log.Printf("Got connection: '%s'", r.Proto)
-}
-
-func kevin(w rest.ResponseWriter, r *rest.Request) {
-
-	w.WriteJson(map[string]string{"Booty": "Where is it?!!"})
-}
-
-func compose(handlerList []httpHandler) httpHandler {
-
-	return func(w rest.ResponseWriter, r *rest.Request) {
-
-		for _, handler := range handlerList {
-
-			handler(w, r)
-		}
-	}
 }
---
+++ rest/rest.go
@@ -0,0 +1,38 @@
+package rest
+
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"log"
+)
+
+type HttpHandler func(rest.ResponseWriter, *rest.Request)
+
+func logger(w rest.ResponseWriter, r *rest.Request) {
+
+	log.Printf("Got connection: '%s'", r.Proto)
+}
+
+func kevin(w rest.ResponseWriter, r *rest.Request) {
+
+	w.WriteJson(map[string]string{"Booty": "Where is it?!!"})
+}
+
+func compose(handlerList []HttpHandler) HttpHandler {
+
+	return func(w rest.ResponseWriter, r *rest.Request) {
+
+		for _, handler := range handlerList {
+
+			handler(w, r)
+		}
+	}
+}
+
+func MakeRouter() (api *rest.Api, err error) {
+
+	api = rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	api.SetApp(rest.AppSimple(compose([]HttpHandler{logger, kevin})))
+
+	return
+}
