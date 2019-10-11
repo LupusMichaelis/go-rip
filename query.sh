62e37eb15c16e09a914006c9a0b50fea419736a0
Mickaël Wolff <m@lupusmic.org>
2019-10-11 14:37:18 +0200

    Use jq for payloads

--- query.sh
+++ query.sh
@@ -3,4 +3,4 @@
 gql=$(cat query.gql)
 query=$(jq -n --arg gql "$gql" '{ "query": $gql }')
 
-curl -k -XPOST -d "$query" https://[::1]:4343/graph; echo
+curl -k -XPOST -d "$query" https://[::1]:4343/graph | jq .
