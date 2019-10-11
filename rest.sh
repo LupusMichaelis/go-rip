f06aef4c8b325b54534714afc9514c23e454a4eb
Mickaël Wolff <m@lupusmic.org>
2019-10-11 10:56:36 +0200

    Some rest cURL for testing

---
+++ rest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -X GET -i -k https://[::1]:4343/rest/country/en
+
+curl \
+    -ik \
+    -X POST \
+    -H 'Content-Type: application/json; charset=UTF-8' \
+    -d '{"code": "to", "Name": "Togo"}' \
+    https://[::1]:4343/rest/country
+
