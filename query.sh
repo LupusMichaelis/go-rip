cb60aa7d407063c151c3881a5241085423dd3fd0
Mickaël Wolff <m@lupusmic.org>
2019-10-14 16:47:49 +0200

    Test on GraphQL mutations

--- query.sh
+++ query.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-gql=$(cat query.gql)
-query=$(jq -n --arg gql "$gql" '{ "query": $gql }')
+gql-query-from-file()
+{
+    local file=$1
+    local gql=$(cat "$file")
 
-curl -k -XPOST -d "$query" https://[::1]:4343/graph | jq .
+    gql-query "$gql"
+}
+
+gql-query()
+{
+    local gql_string=$1
+
+    query=$(jq -n \
+        --arg query_gql "$gql_string" \
+        '{ "query": $query_gql }' \
+    )
+
+    curl -k -XPOST -d "$query" https://[::1]:4343/graph | jq .
+}
+
+gql-query-from-file query.gql
+gql-query-from-file mutate.gql
+
+echo
