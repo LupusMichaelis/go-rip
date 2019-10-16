15be41aa599c31264c7e4412f1911a4e775968db
Mickaël Wolff <m@lupusmic.org>
2019-10-16 10:18:40 +0200

    Add operationName in query examples #wip

--- query.sh
+++ query.sh
@@ -5,22 +5,46 @@ gql-query-from-file()
     local file=$1
     local gql=$(cat "$file")
 
-    gql-query "$gql"
+    local operationName=$2
+    gql-query "$gql" "$operationName"
 }
 
 gql-query()
 {
     local gql_string=$1
+    local operationName=$2
 
-    query=$(jq -n \
-        --arg query_gql "$gql_string" \
-        '{ "query": $query_gql }' \
-    )
+    if [[ -z "$operationName" ]]
+    then
+        query=$(jq -n \
+            --arg query_gql "$gql_string" \
+            '{ "query": $query_gql }' \
+        )
+    else
+        query=$(jq -n \
+            --arg query_gql "$gql_string" \
+            --arg query_operation "$operationName" \
+            '{ "query": $query_gql, "operationName": $query_operation }' \
+        )
+    fi
 
-    curl -k -XPOST -d "$query" https://[::1]:4343/g | jq .
+    echo $query
 }
 
-gql-query-from-file query.gql
-gql-query-from-file mutate.gql
+gql-query-call()
+{
+    local query="$1"
+    curl -k \
+        -H 'Content-type: application/graphql; charset=UTF-8' \
+        -d "$query" https://[::1]:4343/g
+}
+
+gql-query-call "$(gql-query-from-file query.gql)" #| jq .
+echo
+gql-query-call "$(gql-query-from-file mutate.gql)" #| jq .
+echo
+
+#query="$(gql-query-from-file query.gql read) $(gql-query-from-file mutate.gql write)"
+#gql-query-call "$query" | jq .
 
 echo
