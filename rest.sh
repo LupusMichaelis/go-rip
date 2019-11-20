9f29d5dbfa2facbee7237bbe14bfe51452c784bd
Mickaël Wolff <m@lupusmic.org>
2019-11-20 10:24:53 +0100

    Rest test can be called on various hosts

--- rest.sh
+++ rest.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-curl -X GET -i -k https://[::1]:4343/r/country/en; echo
-curl -X GET -i -k https://[::1]:4343/r/country/uk; echo
+host='[::1]'
+port=4343
+
+host='172.17.0.2'
+port=443
+
+curl -X GET -i -k https://"$host":$port/r/country/en; echo
+curl -X GET -i -k https://"$host":$port/r/country/uk; echo
 
 curl \
     -ik \
     -X POST \
     -H 'Content-Type: application/json; charset=UTF-8' \
     -d '{"code": "to", "Name": "Toto"}' \
-    https://[::1]:4343/r/country
+    https://"$host":$port/r/country
 echo
 
 
@@ -17,5 +23,5 @@ curl \
     -X PUT \
     -H 'Content-Type: application/json; charset=UTF-8' \
     -d '{"code": "to", "Name": "Togo"}' \
-    https://[::1]:4343/r/country/to
+    https://"$host":$port/r/country/to
 echo
